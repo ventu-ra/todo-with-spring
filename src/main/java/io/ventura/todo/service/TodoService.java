cdf00b78ffc5431373bc0e406ef76a0789eb7ffb
ventu-ra <ventura09.10.0@gmail.com>
2025-07-01 19:55:04 -0300

    Add TodoService class for managing Todo operations

---
+++ src/main/java/io/ventura/todo/service/TodoService.java
@@ -0,0 +1,34 @@
+package io.ventura.todo.service;
+
+import java.util.List;
+
+import io.ventura.todo.entity.Todo;
+import io.ventura.todo.repository.TodoRepository;
+
+public class TodoService {
+  private final TodoRepository todoRepository;
+
+  public TodoService(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  public List<Todo> create(Todo todo) {
+    todoRepository.save(todo);
+
+    return list();
+  }
+
+  public List<Todo> list() {
+    return todoRepository.findAll();
+  }
+
+  public List<Todo> update(Todo todo) {
+    todoRepository.save(todo);
+    return list();
+  }
+
+  public List<Todo> delete(Long id) {
+    todoRepository.deleteById(id);
+    return list();
+  }
+}
